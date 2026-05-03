d496c91aca2df3224d971e45681707a4e558a688
fzh0919 <fzh0919@hotmail.com>
2026-05-03 19:34:58 +0100

    run.bat update

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
 start http://localhost:8501
-start /min "" D:\Python\python.exe -m streamlit run "%~dp0app.py" --server.enableCORS=false --server.enableXsrfProtection=false --browser.gatherUsageStats=false --server.headless=true
+start /min "" py -3 -m streamlit run "%~dp0app.py" --server.enableCORS=false --server.enableXsrfProtection=false --browser.gatherUsageStats=false --server.headless=true
 exit
